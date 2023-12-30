e258249a2374eea6ad4c322d04b85ddc49d5674d
weibik <j-szkola@wp.pl>
2023-12-30 11:32:03 +0100

    transform the code to click and tqdm

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 from pytube import YouTube
-from sys import argv
+from tqdm import tqdm
+import logging
+import click
 import os
 
-current_folder = os.getcwd()
-target_folder = os.path.join(current_folder, "Download")
+logging.basicConfig(filename='download.log', level=logging.INFO)
 
-link = argv[1]
-yt = YouTube(link)
 
-print(f"Title: {yt.title}")
-print(f"Views {yt.views}")
+def download_with_progress(stream, output):
+    with tqdm(total=stream.filesize, unit='B', unit_scale=True, desc="Downloading") as progress_bar:
+        stream.download(output, filename=stream.default_filename)
+        progress_bar.update(progress_bar.total - progress_bar.n)
 
-yd = yt.streams.get_highest_resolution()
-yd.download(target_folder)
+
+@click.command()
+@click.argument("link", required=True)
+@click.option("--file_format", default="audio", help="Specify the format (audio, video). Default is audio.")
+@click.option('--output', default='Download', help='Specify the output folder.')
+def download_youtube(link, file_format, output):
+    try:
+        os.makedirs(output, exist_ok=True)
+
+        yt = YouTube(link)
+        print(f"Title: {yt.title}")
+        print(f"Views: {yt.views}")
+
+        if file_format == "audio":
+            stream = yt.streams.filter(only_audio=True).first()
+        elif file_format == "video":
+            stream = yt.streams.get_highest_resolution()
+        else:
+            stream = yt.streams.first()
+
+        print("Downloading...")
+        download_with_progress(stream, output)
+        logging.info(f"Downloaded: {yt.title}")
+        click.echo("Download complete.")
+
+    except Exception as e:
+        logging.error(f"Error downloading {link}: {str(e)}")
+        click.echo(f"Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    download_youtube()
