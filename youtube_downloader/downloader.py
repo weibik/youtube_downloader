207cde15a9ca2ad14bbee6bbc2c06140d5e2c021
weibik <j-szkola@wp.pl>
2024-01-02 23:25:01 +0100

    add download with resolution

--- youtube_downloader/downloader.py
+++ youtube_downloader/downloader.py
@@ -5,19 +5,20 @@ import os
 
 
 def download_with_progress(stream, output):
-    with tqdm(total=stream.filesize, unit='B', unit_scale=True, desc="Downloading") as progress_bar:
+    with tqdm(total=stream.filesize, unit="B", unit_scale=True, desc="Downloading") as progress_bar:
         stream.download(output, filename=stream.default_filename)
         progress_bar.update(progress_bar.total - progress_bar.n)
 
 
-def download_file(link, selected_option, output_folder):
+def download_file(link, selected_option, output_folder, resolution):
     try:
         os.makedirs(output_folder, exist_ok=True)
         yt = YouTube(link)
         stream = None
-        if selected_option == 'Video':
-            stream = yt.streams.get_highest_resolution()
-        elif selected_option == 'Audio':
+
+        if selected_option == "Video":
+            stream = yt.streams.filter(type="video", resolution=resolution).first()
+        elif selected_option == "Audio":
             stream = yt.streams.filter(only_audio=True).first()
         else:
             logging.error("Invalid selection")
@@ -26,7 +27,24 @@ def download_file(link, selected_option, output_folder):
             download_with_progress(stream, output_folder)
             logging.info(f"Downloaded: {yt.title}")
         else:
-            logging.error(f"No available stream for {selected_option}")
-
+            logging.error(f"No available stream for {selected_option} and resolution {resolution}")
     except Exception as e:
         logging.error(f"Error downloading {link}: {str(e)}")
+
+
+def get_available_resolutions(link, selected_option):
+    try:
+        yt = YouTube(link)
+        resolutions = set()
+
+        if selected_option == "Video":
+            for stream in yt.streams.filter(type="video"):
+                if stream.resolution:
+                    resolutions.add(stream.resolution)
+        elif selected_option == "Audio":
+            resolutions.add("Audio")
+
+        return list(resolutions)
+    except Exception as e:
+        print(f"Error getting available resolutions: {str(e)}")
+        return []
